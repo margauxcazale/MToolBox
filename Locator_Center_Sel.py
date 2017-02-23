aebf8ad6f5d388cec96d06f757b974f7a892ffba
lcm <lcm@mikrosimage.eu>
2017-02-23 17:22:20 +0100

    bolosse correct

--- Locator_Center_Sel.py
+++ Locator_Center_Sel.py
@@ -6,26 +6,26 @@
 
 import maya.cmds as mc
 
-def locator_center(components)
-components=mc.ls(sl=True, fl=True)
-longueur= len(components)
+def locator_center(components):
+    components=mc.ls(sl=True, fl=True)
+    longueur= len(components)
 
-totx= 0
-toty= 0
-totz= 0
+    totx= 0
+    toty= 0
+    totz= 0
 
-for vtx in components:
-    position= mc.xform(vtx, q=True, t=True, ws=True)
-    print position
-    totx= totx + position[0]
-    toty= toty + position[1]
-    totz= totz + position[2]
+    for vtx in components:
+        position= mc.xform(vtx, q=True, t=True, ws=True)
+        print position
+        totx= totx + position[0]
+        toty= toty + position[1]
+        totz= totz + position[2]
 
-placementX= totx / longueur
-placementY= toty / longueur
-placementZ= totz / longueur
+    placementX= totx / longueur
+    placementY= toty / longueur
+    placementZ= totz / longueur
 
-loc= mc.spaceLocatore(n='centered_locator')
-mc.move(placementX, placementY, placementZ, loc)
+    loc= mc.spaceLocatore(n='centered_locator')
+    mc.move(placementX, placementY, placementZ, loc)
 
 locator_center()
